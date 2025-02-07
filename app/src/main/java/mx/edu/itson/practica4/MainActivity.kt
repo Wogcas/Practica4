d6428ab31e2b410cb3a8245ce24146a05484d640
Wogcas <gael.castro247887@potros.itson.edu.mx>
2025-02-06 18:55:33 -0700

    Backend

--- app/src/main/java/mx/edu/itson/practica4/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/practica4/MainActivity.kt
@@ -1,6 +1,9 @@
 package mx.edu.itson.practica4
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,67 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        fun calcularIMC(kilos:Double, altura:Double): Double{
+            return kilos/(altura*altura)
+        }
+
+        val pesoK = findViewById<EditText>(R.id.etKilos)
+        val alturaE = findViewById<EditText>(R.id.etEstatura)
+        val calcular = findViewById<Button>(R.id.btnCalcular)
+        val imc = findViewById<TextView>(R.id.tvIMC)
+        val rango = findViewById<TextView>(R.id.tvRange)
+
+        calcular.setOnClickListener {
+            var peso: Double = 0.0
+            var estatura: Double = 0.0
+            try{
+                peso = pesoK.text.toString().toDouble()
+                estatura = alturaE.text.toString().toDouble()
+            } catch (e:java.lang.Exception) {
+                imc.setText("Debe ingresar valores reales")
+                print(e)
+            }
+            var resultado = calcularIMC(peso, estatura)
+            val formattedNumber = "%.2f".format(resultado)
+            imc.setText(formattedNumber)
+            var salud: String
+            var color: Int
+
+            when {
+                resultado < 18.5 -> {
+                    salud = "Bajo Peso"
+                    color = R.color.colorRed
+                }
+                resultado >= 18.5 && resultado <= 24.9 -> {
+                    salud = "Saludable"
+                    color = R.color.colorGreen
+                }
+                resultado >= 25 && resultado <= 29.9 -> {
+                    salud = "Sobrepeso"
+                    color = R.color.colorYellow
+                }
+                resultado >= 30 && resultado <= 34.9 -> {
+                    salud = "Obesidad Grado 1"
+                    color = R.color.colorOrange
+                }
+                resultado >=35 && resultado <= 39.9 -> {
+                    salud = "Obesidad Grado 2"
+                    color = R.color.colorBrown
+                }
+                resultado >= 40 -> {
+                    salud = "Obesidad Grado 3"
+                    color = R.color.colorRed
+                }
+                else -> {
+                    salud = "Error"
+                    color = 0
+                }
+            }
+            rango.setBackgroundResource(color)
+            rango.setText(salud)
+
+        }
+
     }
 }
